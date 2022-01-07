423f13c5f7980b019b936eb70f8c8c4daf8d83b2
Alex Oh <oalex@knights.ucf.edu>
2022-01-07 09:53:31 -0500

    CTCI 2.2 Return Kth to Last Iterative Solution

--- Ch2. Linked LIsts/RemoveDuplicates.java
+++ Ch2. Linked LIsts/RemoveDuplicates.java
@@ -1,15 +1,67 @@
 import java.util.*;
 public class RemoveDuplicates{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
 
         Node node = new Node(5);
         node.appendToTail(10);
         node.appendToTail(1);
         node.appendToTail(3);
         node.appendToTail(8);
+        node.appendToTail(1);
+        node.appendToTail(7);
+        node.appendToTail(8);
+        node.appendToTail(15);
+        node.appendToTail(0);
+        node.appendToTail(90);
+        node.appendToTail(10);
+
+        printNode(node);
+        removeDuplicates(node);
+        //removeDuplicatesTradeOff(node);
         printNode(node);
     }
+
+    //This would take O(n) but would require extra space O(n)
+    public static Node removeDuplicates(Node head){
+        Node prev = null;
+        Set<Integer> hs = new HashSet<Integer>();
+        
+        while(head!= null){
+            if(hs.contains(head.data)){
+                prev.next = head.next;
+            }
+            else{
+                hs.add(head.data);
+                //why is prev = head here instead of out of the else statement? 
+                prev = head;
+            }
+            head = head.next;
+        }
+        return head;
+    }
+
+    //This would take O(n^2) but no extra space is required.
+    public static Node removeDuplicatesTradeOff(Node head){
+        //pointer 1 and pointer 2.
+        Node p1 = head;
+
+        while(p1.next != null){
+            Node p2 = p1;
+            while(p2.next != null){
+                if(p1.data == p2.next.data){
+                    p2.next = p2.next.next;
+                }
+                else{
+                    //why is p2 = p2.next here instead of out of the else statement? 
+                    p2 = p2.next;
+                }  
+            }
+            p1 = p1.next;
+        }
+
+        return head;
+    }
+
 
     public static void printNode(Node head){
         if(head == null) return;
@@ -19,6 +71,8 @@ public class RemoveDuplicates{
             head = head.next;
         }
         System.out.print(head.data);
+        System.out.println();
+        System.out.println();
     }
 }
 class Node{
@@ -30,11 +84,11 @@ class Node{
     }
 
     void appendToTail(int d){
-        Node end = new Node(d);
+        Node endNode = new Node(d);
         Node n = this;
-        while(n.next  != null){
+        while(n != null){
             n = n.next;
         }
-        n.next = end;
+        n.next = endNode;
     }
 }
---
+++ Ch2. Linked LIsts/ReturnKthToLast.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class ReturnKthToLast {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+
+        Node node = new Node(1);
+        node.append(2);
+        node.append(3);
+        node.append(4);
+        node.append(5);
+        node.append(6);
+        node.append(7);
+        node.append(8);
+        node.append(9);
+        node.append(10);
+        printNode(node);
+
+        System.out.println(k + "th item to last is " + returnKthToLast(node, k));
+    }
+    public static void printNode(Node head){
+        while(head.next!= null){
+            System.out.print(head.data + " --> ");
+            head = head.next;
+        }
+        System.out.print(head.data);
+        System.out.println();
+        
+    }
+    public static int returnKthToLast(Node head, int k){
+        Node p1 =  head;
+        Node p2 = head;
+
+        for(int i = 0; i < k; i++){
+            p1 = p1.next;
+        }
+        while(p1 != null){
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return p2.data;
+
+    }
+
+
+}
+class Node{
+    Node next = null;
+    int data;
+
+    public Node(int d){
+        data = d;
+
+    }
+
+    void append(int d){
+        Node endNode = new Node(d);
+        Node n = this;
+
+        while(n.next != null){
+            n = n.next;
+        }
+        n.next = endNode;
+    }
+}
